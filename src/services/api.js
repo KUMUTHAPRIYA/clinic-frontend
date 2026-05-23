b7e0c0863b9c9c7cc05179ad15defcb7b27ee731
KUMUTHAPRIYA <kumuthapriya.d@gmail.com>
2026-05-23 17:01:40 +0530

    connected deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "clinic-backend-production-7812.up.railway.app",
 });
 
 export default API;
